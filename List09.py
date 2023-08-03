c4b5c0bce8aa01dc8d9c6e17245048849a055648
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-03 19:07:52 +0500

    apple

--- List09.py
+++ List09.py
@@ -6,4 +6,17 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    sanoq=fruits.count('apple')
+    if fruits[0]=='apple':
+        fruits[0]=0
+    if fruits[1]=='apple':
+        fruits[1]=1
+    if fruits[2]=='apple':
+        fruits[2]=2
+    if fruits[3]=='apple':
+        fruits[3]=3
+    if fruits[4]=='apple':
+        fruits[4]=4
+    list1=[sanoq,fruits[0],fruits[1],fruits[2],fruits[3],fruits[4]]
+    return list1
+print(main(fruits=['apple','kiwi','apple','apple','banan']))
